8b9e901cbddd1e0e64183aa19415109eb1cf6f45
davien21 <chidiebereekennia@gmail.com>
2021-11-17 03:29:22 +0100

    bug fix

--- src/controllers/media.js
+++ src/controllers/media.js
@@ -20,8 +20,9 @@ class MediaResourceController {
   async create(req, res) {
     const { body, file } = req;
     if (!file?.path) throw new BadRequestError("Invalid file path");
-  
-    const url = env.BASE_URL + getFilePath(file.destination) + file.filename;
+
+    const url =
+      env.BASE_URL + "/uploads" + getFilePath(file.destination) + file.filename;
 
     const typeStartIndex = file.filename.lastIndexOf(".") + 1;
     const type = file.filename.slice(typeStartIndex);
