63c3a87075316d842f1ba9694e80d79b2d4a7c24
davien21 <chidiebereekennia@gmail.com>
2021-11-17 03:26:58 +0100

    bug fix

--- src/config/routing.js
+++ src/config/routing.js
@@ -13,7 +13,7 @@ module.exports = (app) => {
   app.use(morgan("dev"));
 
   app.use(express.static("public"));
-  app.use("/api/v1", express.static("uploads"));
+  app.use("/api/v1/uploads", express.static("uploads"));
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
 
